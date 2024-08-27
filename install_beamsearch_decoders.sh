851a9dec6810e4116ba14bf7b4bb7f369debf656
Mahmoud ghareeb <mahmoudghareeb11111@gmail.com>
2024-08-28 01:54:04 +0300

    Update install_beamsearch_decoders.sh
    Signed-off-by: Mahmoud ghareeb <mahmoudghareeb11111@gmail.com>

--- install_beamsearch_decoders.sh
+++ install_beamsearch_decoders.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
-# Use this script to install KenLM, OpenSeq2Seq decoder
 shopt -s expand_aliases
 
 NEMO_PATH=$(pwd)
 if [ "$#" -eq 1 ]; then
   NEMO_PATH=$1
 fi
-KENLM_MAX_ORDER=10 # Maximum order of KenLM model, also specified in the setup_os2s_decoders.py
+KENLM_MAX_ORDER=10
 
 if [ -d "$NEMO_PATH" ]; then
   echo "The folder '$NEMO_PATH' exists."
@@ -28,7 +27,7 @@ fi
 aptupdate && apt-get upgrade -y && apt-get install -y swig liblzma-dev && rm -rf /var/lib/apt/lists/* # liblzma needed for flashlight decoder
 
 # install Boost package for KenLM
-#wget https://boostorg.jfrog.io/artifactory/main/release/1.80.0/source/boost_1_80_0.tar.bz2 --no-check-certificate && tar --bzip2 -xf $NEMO_PATH/boost_1_80_0.tar.bz2 && cd boost_1_80_0 && ./bootstrap.sh && b2install --layout=tagged link=static,shared threading=multi,single install -j4 && cd .. || echo FAILURE
+wget https://boostorg.jfrog.io/artifactory/main/release/1.80.0/source/boost_1_80_0.tar.bz2 --no-check-certificate && tar --bzip2 -xf $NEMO_PATH/boost_1_80_0.tar.bz2 && cd boost_1_80_0 && ./bootstrap.sh && b2install --layout=tagged link=static,shared threading=multi,single install -j4 && cd .. || echo FAILURE
 export BOOST_ROOT=$NEMO_PATH/boost_1_80_0
 
 git clone https://github.com/NVIDIA/OpenSeq2Seq
@@ -41,12 +40,16 @@ cd $NEMO_PATH/decoders
 cp $NEMO_PATH/scripts/installers/setup_os2s_decoders.py ./setup.py
 ./setup.sh
 
-# install KenLM
 cd $NEMO_PATH/decoders/kenlm/build && cmake -DKENLM_MAX_ORDER=$KENLM_MAX_ORDER .. && make -j2
 cd $NEMO_PATH/decoders/kenlm
 python setup.py install --max_order=$KENLM_MAX_ORDER
 export KENLM_LIB=$NEMO_PATH/decoders/kenlm/build/bin
 export KENLM_ROOT=$NEMO_PATH/decoders/kenlm
+cd ..
+
+git clone https://github.com/flashlight/text && cd text
+python setup.py bdist_wheel
+pip install dist/*.whl
 cd ..
 
 #setup the folder
